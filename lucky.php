5b5b017bea81cd3bf10f857e6b5a3f4ee8d9e327
MohdZairi <mohdzairi658@gmail.com>
2022-10-25 09:16:46 +0800

    create id to display name user

--- lucky.php
+++ lucky.php
@@ -34,11 +34,12 @@
     <!-- Page Header -->
     <header class="header">
         <div class="overlay">
-            <h1 class="subtitle">Congratulations</h1> 
+            <h1 class="subtitle">Congratulations</h1>
+            <div id=nameuser>
+                <h1 class="title">Welcome To Lucky Draw</h1>  
+            </div> 
         </div>
-        <div class="overlay" id=nameuser>
-            <h1 class="title">Welcome To Lucky Draw</h1>  
-        </div>    
+    
         <div class="shape">
             <svg viewBox="0 0 1500 200">
                 <path d="m 0,240 h 1500.4828 v -71.92164 c 0,0 -286.2763,-81.79324 -743.19024,-81.79324 C 300.37862,86.28512 0,168.07836 0,168.07836 Z"/>
